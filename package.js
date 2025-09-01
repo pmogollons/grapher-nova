aff7e0f03ca1190734395b932ec7b96983794056
Paulo Mogollón <pmogollon@propiedata.com>
2025-09-01 13:21:04 -0500

    Published v2.0.5

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "pmogollons:nova",
-  version: "2.0.4",
+  version: "2.0.5",
   summary: "Grapher-like query layer on top of nova and meteor",
   git: "https://github.com/pmogollons/grapher-nova",
   documentation: "README.md",
